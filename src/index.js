5c56c6a9a03e1b0efd8c399dd9b9127c60c55f50
frederikgdl <frederikgdl@gmail.com>
2018-06-02 12:09:54 +0200

    Remove fragment

--- src/index.js
+++ src/index.js
@@ -13,9 +13,7 @@ const target = document.querySelector('#root')
 render(
     <Provider store={store}>
         <ConnectedRouter history={history}>
-            <React.Fragment>
-                <App />
-            </React.Fragment>
+            <App />
         </ConnectedRouter>
     </Provider>,
     target
