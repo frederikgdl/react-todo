94ec87f515958958deb78031b65d437ac39793cd
frederikgdl <frederikgdl@gmail.com>
2018-06-03 18:48:10 +0200

    Add not found component

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
-import NewPage from '../components/pages/new/NewPage'
+import { Route, Switch } from 'react-router-dom'
 import ListPage from '../components/pages/list/ListPage'
+import NewPage from '../components/pages/new/NewPage'
+import { NotFound } from '../components/pages/NotFound'
 
 export default () => (
     <div className="container">
-        <Route exact path="/" component={ListPage} />
-        <Route exact path="/new" component={NewPage} />
+        <Switch>
+            <Route exact path="/" component={ListPage} />
+            <Route exact path="/new" component={NewPage} />
+            <Route component={NotFound} />
+        </Switch>
     </div>
 )
