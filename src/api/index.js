c94b7d37018b816d20dd56a9e6b96ab62174e945
Gorvell de Lichtenberg, Frederik <frederik.gorvell-de-lichtenberg@nav.no>
2018-06-13 15:46:52 +0200

    Change server port and rename yml-file

--- src/api/index.js
+++ src/api/index.js
@@ -15,7 +15,7 @@ import {
     onAddItemFailure
 } from '../actions'
 
-const serverUrl = 'http://localhost:4000'
+const serverUrl = 'http://localhost:8080'
 const itemsUrl = `${serverUrl}/items`
 
 export const fetchItems = () => async dispatch => {
