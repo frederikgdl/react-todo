59394c55b06b9b4b50b34df585f3d09c1782bfd3
Gorvell de Lichtenberg, Frederik <frederik.gorvell-de-lichtenberg@nav.no>
2018-06-15 09:23:51 +0200

    Change header to react todo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => (
     <nav className="navbar is-dark">
         <div className="navbar-brand">
-            <div className="navbar-item">Elm Todo</div>
+            <div className="navbar-item">React Todo</div>
         </div>
     </nav>
 )
